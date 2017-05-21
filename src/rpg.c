210135c0c0a25b6720512eab265ddae7dcb9db13
Ankit Pati <contact@ankitpati.in>
2017-05-21 17:58:30 +0530

    add more options, rename macros

--- src/rpg.c
+++ src/rpg.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ASCII_READABLE_LOWER 0x21
-#define ASCII_READABLE_UPPER 0x7e
 size_t lrand(size_t rand, size_t lower, size_t upper)
 {
     return lower > upper ? rand % (lower - upper + 1) + upper :
@@ -31,19 +29,48 @@ unsigned char csprng()
     return number;
 }
 
-#define DEFAULT_LENGTH_LOWER  8
-#define DEFAULT_LENGTH_UPPER 16
+#define PRINTABLE_MIN      0x21
+#define PRINTABLE_MAX      0x7e
+#define DEFAULT_MIN_LENGTH    8
+#define DEFAULT_MAX_LENGTH   16
 int main(int argc, char **argv)
 {
-    size_t length;
+    size_t min, max, howmany, length;
 
-    length = argc == 2 ? strtoull(argv[1], NULL, 10) :
-                    lrand(csprng(), DEFAULT_LENGTH_LOWER, DEFAULT_LENGTH_UPPER);
+    howmany = 1;
 
-    while (length--)
-        putchar(lrand(csprng(), ASCII_READABLE_LOWER, ASCII_READABLE_UPPER));
+    switch (argc) {
+    case 1:
+        min = DEFAULT_MIN_LENGTH;
+        max = DEFAULT_MAX_LENGTH;
+        break;
 
-    putchar('\n');
+    case 2:
+        min = max = strtoull(argv[1], NULL, 10);
+        break;
+
+    case 3:
+        min = strtoull(argv[1], NULL, 10);
+        max = strtoull(argv[2], NULL, 10);
+        break;
+
+    case 4:
+        min = strtoull(argv[1], NULL, 10);
+        max = strtoull(argv[2], NULL, 10);
+        howmany = strtoull(argv[3], NULL, 10);
+        break;
+
+    default:
+        fprintf(stderr, "Usage:\n\trpg [min-length] max-length [how-many]\n");
+        exit(1);
+        break;
+    }
+
+    while (howmany--) {
+        length = lrand(csprng(), min, max);
+        while (length--) putchar(lrand(csprng(), PRINTABLE_MIN, PRINTABLE_MAX));
+        putchar('\n');
+    }
 
     return 0;
 }
