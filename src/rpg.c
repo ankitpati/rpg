9b36899d8d3c0301d30906d1ae7b04714178e176
Ankit Pati <contact@ankitpati.in>
2017-05-21 13:51:05 +0530

    refactored to make code more self-documenting

--- src/rpg.c
+++ src/rpg.c
@@ -1,37 +1,37 @@
 /* rpg.c */
 /* Random Password Generator */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEFAULT_LENGTH_LOWER  8
-#define DEFAULT_LENGTH_UPPER 16
-
 #define ASCII_READABLE_LOWER 0x21
 #define ASCII_READABLE_UPPER 0x7e
-
 size_t lrand(size_t rand, size_t lower, size_t upper)
 {
     return rand % (upper - lower + 1) + lower;
 }
 
+#define RANDOM_DEVICE "/dev/urandom"
 size_t csprng()
 {
     static FILE *fp;
     size_t number;
 
-    if (!fp && !(fp = fopen("/dev/urandom", "rb"))) {
-        fprintf(stderr, "Could not open /dev/urandom.\n");
+    if (!fp && !(fp = fopen(RANDOM_DEVICE, "rb"))) {
+        fprintf(stderr, "Could not open " RANDOM_DEVICE "\n");
         exit(5);
     }
 
     if (fread(&number, sizeof(number), 1, fp) != 1) {
-        fprintf(stderr, "I/O error while reading from /dev/urandom.\n");
+        fprintf(stderr, "I/O error while reading from " RANDOM_DEVICE "\n");
         exit(5);
     }
 
     return number;
 }
 
+#define DEFAULT_LENGTH_LOWER  8
+#define DEFAULT_LENGTH_UPPER 16
 int main(int argc, char **argv)
 {
     size_t length;
