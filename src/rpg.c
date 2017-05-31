b5ed35a954ded2f16259861bd6cd6e7c3fcee319
Ankit Pati <contact@ankitpati.in>
2017-05-31 21:38:14 +0530

    Plug Entropy Loss, Improve Performance, Increase Range of Randomness
    Limit entropy loss to a maximum of (8*sizeof(size_t) - 1) bits per invocation;
    Buffer RANDOM_DEVICE reads to reduce I/O calls;
    size_t has greater range than previously used unsigned char.

--- src/rpg.c
+++ src/rpg.c
@@ -5,14 +5,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+size_t bitcount(size_t num)
+{
+    size_t c;
+    for (c = 0; num; ++c) num >>= 1;
+    return c;
+}
+
 size_t lrand(size_t rand, size_t lower, size_t upper)
 {
-    return lower > upper ? rand % (lower - upper + 1) + upper :
-                           rand % (upper - lower + 1) + lower ;
+    return rand % (upper - lower + 1) + lower ;
 }
 
 #define RANDOM_DEVICE "/dev/urandom"
-unsigned char csprng()
+size_t csprng(size_t bits)
 {
 #ifdef _WIN32
 
@@ -28,19 +34,36 @@ unsigned char csprng()
 #else
 
     static FILE *fp;
-    unsigned char number;
+    static size_t bits_left, buf;
+    size_t ret, shift;
 
     if (!fp && !(fp = fopen(RANDOM_DEVICE, "rb"))) {
         fprintf(stderr, "Could not open " RANDOM_DEVICE "\n");
         exit(5);
     }
 
-    if (fread(&number, sizeof(number), 1, fp) != 1) {
-        fprintf(stderr, "I/O error while reading from " RANDOM_DEVICE "\n");
-        exit(5);
+    if (bits > 8*sizeof(buf)) bits = 8*sizeof(buf);
+
+    ret = 0;
+
+    if (bits_left < bits) {
+        ret = buf;
+        ret <<= bits - bits_left;
+        bits = bits - bits_left;
+
+        if (fread(&buf, sizeof(buf), 1, fp) != 1) {
+            fprintf(stderr, "I/O error while reading from " RANDOM_DEVICE "\n");
+            exit(5);
+        }
+        bits_left = 8*sizeof(buf);
     }
 
-    return number;
+    shift = 8*sizeof(buf) - bits;
+    ret |= buf << shift >> shift;
+    buf >>= bits;
+    bits_left -= bits;
+
+    return ret;
 
 #endif
 }
@@ -49,6 +72,7 @@ unsigned char csprng()
 #define PRINTABLE_MAX      0x7e
 #define DEFAULT_MIN_LENGTH    8
 #define DEFAULT_MAX_LENGTH   16
+#define SWAP(a, b) do (a) ^= (b), (b) ^= (a), (a) ^= (b); while (0)
 int main(int argc, char **argv)
 {
     size_t min, max, howmany, length;
@@ -82,9 +106,18 @@ int main(int argc, char **argv)
         break;
     }
 
+    if (min > max) SWAP(min, max);
+
     while (howmany--) {
-        length = lrand(csprng(), min, max);
-        while (length--) putchar(lrand(csprng(), PRINTABLE_MIN, PRINTABLE_MAX));
+        length = lrand(csprng(bitcount(max - min)), min, max);
+        while (length--)
+            putchar(
+                lrand(
+                    csprng(bitcount(PRINTABLE_MAX - PRINTABLE_MIN)),
+                    PRINTABLE_MIN, PRINTABLE_MAX
+                )
+            );
+
         putchar('\n');
     }
 
